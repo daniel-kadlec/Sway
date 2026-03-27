6bcb2cbf077aa1fa0119b24ff940c2fbf6d2bab9
Decayyer <daniel-kadlec@seznam.cz>
2026-03-27 15:18:50 +0100

    Refactor: Moved pages to pages components to support SSR, added login files

---
+++ app/pages/tablePage.tsx
@@ -0,0 +1,67 @@
+'use client';
+import {useModal} from "@/app/Context/ModalContext";
+
+import { FaSort } from "react-icons/fa6";
+import { FaSortUp } from "react-icons/fa";
+import { FaSortDown } from "react-icons/fa6";
+
+
+export default function TablePage() {
+    const { openModal } = useModal();
+
+    const columns = [
+        "Name",
+        "E-mail",
+        "IG username",
+        "Phone",
+        "URL",
+        "Primary contact",
+        "Primary follow up",
+        "Secondary contact",
+        "Secondary follow up",
+        "Stage",
+        "Verdict",
+    ];
+
+    return (
+
+        <div className="bg-white rounded-3xl shadow-primary overflow-x-auto h-full custom-scrollbar">
+            <table className="w-full text-left border-collapse text-2xl">
+                {/* Header */}
+                <thead className="bg-primary text-offwhite">
+                <tr>
+                    {columns.map((col) => (
+                        <th key={col} className="table-cell font-bold whitespace-nowrap">
+                            <span className="flex items-center gap-2 cursor-pointer">
+                                {col}
+                                <FaSort className="size-6 mt-0.5"/>
+                            </span>
+                        </th>
+                    ))}
+                </tr>
+                </thead>
+
+                {/* body */}
+                <tbody>
+                <tr onClick={() => openModal("view")} className="border-b border-b-lightgray transition duration-300 cursor-pointer hover:bg-lightgray ">
+                    <td className="font-bold text-primary table-cell">John Doe</td>
+                    <td className="table-cell">john@example.com</td>
+                    <td className="table-cell">johndoe</td>
+                    <td className="table-cell">123 456 789</td>
+                    <td className="table-cell">johndoe.com</td>
+
+                    <td className="table-cell">2026-03-25</td>
+                    <td className="table-cell">2026-04-2</td>
+
+
+                    <td className="table-cell">2026-03-25</td>
+                    <td className="table-cell">2026-03-28</td>
+
+                    <td className="table-cell">Primary follow-up</td>
+                    <td className="table-cell">In progresswwwwwwwwwwwww </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
