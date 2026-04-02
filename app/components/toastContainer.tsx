00b8b155681a68de850cfd5750e0e6481885ab38
Decayyer <daniel-kadlec@seznam.cz>
2026-04-02 13:36:25 +0200

    Feat: enhance toast animations and styling

--- app/components/toastContainer.tsx
+++ app/components/toastContainer.tsx
@@ -12,29 +12,30 @@ type Props = {
 
 export default function ToastContainer({ toasts }: Props) {
     return (
-        <div className="fixed top-4 left-1/2 -translate-x-1/2 flex flex-col gap-2 z-[999999]">
+        <div className="fixed top-4 left-1/2 -translate-x-1/2 flex flex-col gap-2 z-999999">
             {toasts.map((toast) => (
                 // Toast
                 <div
                     key={toast.id}
-                    className="bg-offwhite text-offblack relative px-5 py-3 rounded-xl overflow-hidden shadow-primary flex gap-3 justify-center items-center"
+                    className="bg-white text-offblack relative px-6 py-3 rounded-xl overflow-hidden shadow-primary flex justify-center items-center toastPresence"
                 >
-                    <div className={'bg-error-light p-1 rounded-full mt-0.5'}>
-                        <IoClose className={'text-error w-6 h-6'}/>
-                    </div>
-                    <div className={'flex flex-col justify-center items-start'}>
+                    <div className={'flex gap-3 justify-center items-center mb-1'}>
+                        <div className={'bg-error-light p-1 rounded-full mt-0.5'}>
+                            <IoClose className={'text-error w-6 h-6'}/>
+                        </div>
+                        <div className={'flex flex-col justify-center items-start'}>
                         <span className={'font-bold -mb-2 text-xl'}>
                             {toast.message}
                         </span>
-                        {toast.description &&
-                            <span className={'text-lg'}>
+                            {toast.description &&
+                                <span className={'text-lg'}>
                             {toast.description}
                         </span>
-                        }
+                            }
+                        </div>
                     </div>
-                    <div className={'bottom-0 left-0 w-full h-1 bg-error absolute toastCountdown duration-300'}>
 
-                    </div>
+                    <div className={'bottom-0 left-0 w-full h-1 bg-error absolute toastCountdown'}/>
                 </div>
             ))}
         </div>
