9ff64222f1733da681172a38ef986a085ec404f6
Decayyer <daniel-kadlec@seznam.cz>
2026-04-07 19:18:47 +0200

    Fix: Create modal container layering

--- app/components/Modal/createModal.tsx
+++ app/components/Modal/createModal.tsx
@@ -52,103 +52,101 @@ export default function CreateModal({ data }: CreateModalProps) {
     };
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
-            <div className="modal-container">
+        <div className="">
 
-                {/* Header */}
-                <div className="flex items-center justify-between mb-8">
-                    <h2 className="text-4xl font-bold text-primary">
-                        Create a new lead
-                    </h2>
+            {/* Header */}
+            <div className="flex items-center justify-between mb-8">
+                <h2 className="text-4xl font-bold text-primary">
+                    Create a new lead
+                </h2>
 
-                    <button
-                        onClick={() => closeModal()}
-                        className="size-12 flex items-center justify-center rounded-full bg-primary-light transition cursor-pointer hover:opacity-90"
-                    >
-                       <IoClose className={'text-primary size-8'}/>
-                    </button>
+                <button
+                    onClick={() => closeModal()}
+                    className="size-12 flex items-center justify-center rounded-full bg-primary-light transition cursor-pointer hover:opacity-90"
+                >
+                   <IoClose className={'text-primary size-8'}/>
+                </button>
+            </div>
+
+            {/* Advanced toggle */}
+            <div className="flex justify-end">
+                <label className="flex items-center gap-2 text-xl cursor-pointer font-bold">
+                    <input
+                        className={'size-5'}
+                        type="checkbox"
+                        checked={advanced}
+                        onChange={() => setAdvanced(!advanced)}
+                    />
+                    Advanced mode
+                </label>
+            </div>
+
+            <div className="grid grid-cols-2 gap-8">
+
+                <div className="col-span-2">
+                    <Label>Company name</Label>
+                    <Input onChange={(e) => handleInputChange("companyName", e.target.value)} placeholder="Velká firma" />
                 </div>
 
-                {/* Advanced toggle */}
-                <div className="flex justify-end">
-                    <label className="flex items-center gap-2 text-xl cursor-pointer font-bold">
-                        <input
-                            className={'size-5'}
-                            type="checkbox"
-                            checked={advanced}
-                            onChange={() => setAdvanced(!advanced)}
-                        />
-                        Advanced mode
-                    </label>
+                <div>
+                    <Label>Primary contact</Label>
+                    <Input onChange={(e) => handleInputChange("primaryContact", e.target.value)} placeholder="E-mail, IG, phone" />
                 </div>
 
-                <div className="grid grid-cols-2 gap-8">
-
-                    <div className="col-span-2">
-                        <Label>Company name</Label>
-                        <Input onChange={(e) => handleInputChange("companyName", e.target.value)} placeholder="Velká firma" />
-                    </div>
-
-                    <div>
-                        <Label>Primary contact</Label>
-                        <Input onChange={(e) => handleInputChange("primaryContact", e.target.value)} placeholder="E-mail, IG, phone" />
-                    </div>
-
-                    <div>
-                        <Label>Primary platform</Label>
-                        <Select value={form.primaryPlatform}
-                                onChange={(e) =>
-                                    handleInputChange("primaryPlatform", e.target.value)
-                                }/>
-                    </div>
-
-                    <div>
-                        <Label>Website (optional)</Label>
-                        <Input onChange={(e) => handleInputChange("website", e.target.value)} placeholder="youtube.com" />
-                    </div>
-
-                    <div>
-                        <Label>Contact date (optional)</Label>
-                        <Input
-                            type="date"
-                            value={form.contactDate}
-                            onChange={(e) => handleInputChange("contactDate", e.target.value)} placeholder="7.7.2027" />
-                    </div>
-
-                    {advanced && (
-                        <>
-                            <div>
-                                <Label>Secondary contact</Label>
-                                <Input onChange={(e) => handleInputChange("secondaryContact", e.target.value)} placeholder="E-mail, IG, phone" />
-                            </div>
-
-                            <div>
-                                <Select value={form.primaryPlatform}
-                                        onChange={(e) =>
-                                            handleInputChange("secondaryPlatform", e.target.value)
-                                        }/>
-                            </div>
-
-                            <div className="col-span-2">
-                                <Label>Note</Label>
-                                <textarea
-                                    className="w-full min-h-[100px] rounded-xl border border-lightgray px-4 py-3 text-lg focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Poznámka..."
-                                    onChange={(e) => handleInputChange("note", e.target.value)}
-                                />
-                            </div>
-                        </>
-                    )}
+                <div>
+                    <Label>Primary platform</Label>
+                    <Select value={form.primaryPlatform}
+                            onChange={(e) =>
+                                handleInputChange("primaryPlatform", e.target.value)
+                            }/>
                 </div>
 
-                <div className="flex justify-end gap-3 mt-10">
-                    <Button className={'bg-primary-light! text-primary!'}>
-                        Create & Next
-                    </Button>
-                    <Button onClick={handleLeadCreate}>
-                        Create
-                    </Button>
+                <div>
+                    <Label>Website (optional)</Label>
+                    <Input onChange={(e) => handleInputChange("website", e.target.value)} placeholder="youtube.com" />
                 </div>
+
+                <div>
+                    <Label>Contact date (optional)</Label>
+                    <Input
+                        type="date"
+                        value={form.contactDate}
+                        onChange={(e) => handleInputChange("contactDate", e.target.value)} placeholder="7.7.2027" />
+                </div>
+
+                {advanced && (
+                    <>
+                        <div>
+                            <Label>Secondary contact</Label>
+                            <Input onChange={(e) => handleInputChange("secondaryContact", e.target.value)} placeholder="E-mail, IG, phone" />
+                        </div>
+
+                        <div>
+                            <Select value={form.primaryPlatform}
+                                    onChange={(e) =>
+                                        handleInputChange("secondaryPlatform", e.target.value)
+                                    }/>
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>Note</Label>
+                            <textarea
+                                className="w-full min-h-[100px] rounded-xl border border-lightgray px-4 py-3 text-lg focus:outline-none focus:ring-2 focus:ring-primary"
+                                placeholder="Poznámka..."
+                                onChange={(e) => handleInputChange("note", e.target.value)}
+                            />
+                        </div>
+                    </>
+                )}
+            </div>
+
+            <div className="flex justify-end gap-3 mt-10">
+                <Button className={'bg-primary-light! text-primary!'}>
+                    Create & Next
+                </Button>
+                <Button onClick={handleLeadCreate}>
+                    Create
+                </Button>
             </div>
         </div>
     );
