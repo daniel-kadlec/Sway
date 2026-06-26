5b3e9c2277785fd3b70e5740d7a155248765fbf6
Decayyer <daniel-kadlec@seznam.cz>
2026-06-26 15:03:37 +0200

    Feat: Added decorative circles to login page background

--- views/loginPage.tsx
+++ views/loginPage.tsx
@@ -1,12 +1,14 @@
 import LoginForm from "@/components/loginForm";
 import Image from 'next/image'
 import logo from "@/public/logo.svg";
-
+import circles from "@/public/circles.svg";
 export default async function LoginPage() {
 
     return(
-        <div className={'gradient-primary min-h-screen flex justify-center items-center'}>
-            <div className={"flex flex-col gap-4 items-start justify-center h-screen -mt-12"}>
+        <div className={'gradient-primary min-h-screen flex justify-center items-center relative overflow-hidden'}>
+            <Image src={circles} alt={"Circle decoration"} className={'absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2'}/>
+            <Image src={circles} alt={"Circle decoration"} className={'absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2'}/>
+            <div className={"flex flex-col gap-4 items-start justify-center h-screen -mt-12 z-100"}>
                 <Image src={logo} alt={"Logo"} className={'w-[65%] mx-auto mb-4'}/>
                 <div className={"flex flex-col items-start text-offwhite text-xl"}>
                     <h1 className={'font-bold -mb-1'}>Continue the flow.</h1>
