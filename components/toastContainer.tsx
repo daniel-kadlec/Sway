8813e3c263e790ac9df0e344832b0ab3a3916a07
Decayyer <daniel-kadlec@seznam.cz>
2026-09-02 21:20:00 +0200

    Feat: Toast redesign

--- components/toastContainer.tsx
+++ components/toastContainer.tsx
@@ -2,6 +2,7 @@ import { FaCheck } from "react-icons/fa";
 import { IoIosWarning } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import { ToastType } from "@/context/ToastContext";
+import { AnimatePresence, motion } from "motion/react";
 
 type Props = {
     toasts: ToastType[];
@@ -9,23 +10,23 @@ type Props = {
 
 const variants = {
     success: {
-        bgLight: "bg-success-light",
+        bgLight: "bg-success/30",
         bg: "bg-success",
-        iconColor: "text-success",
+        iconColor: "text-offblack",
         icon: FaCheck,
-        iconSize: 18,
+        iconSize: 14,
     },
     warning: {
-        bgLight: "bg-warning-light",
+        bgLight: "bg-warning/30",
         bg: "bg-warning",
-        iconColor: "text-warning",
+        iconColor: "text-offblack",
         icon: IoIosWarning,
         iconSize: 20,
     },
     error: {
-        bgLight: "bg-error-light",
+        bgLight: "bg-error/30",
         bg: "bg-error",
-        iconColor: "text-error",
+        iconColor: "text-offblack",
         icon: IoClose,
         iconSize: 24,
     },
@@ -33,41 +34,70 @@ const variants = {
 
 export default function ToastContainer({ toasts }: Props) {
     return (
-        <div className="fixed top-4 left-1/2 -translate-x-1/2 flex flex-col gap-2 z-999999">
-            {toasts.map((toast) => {
-                const variant = variants[toast.type];
-                const Icon = variant.icon;
+            <div className="fixed top-4 right-4 flex flex-col gap-2 z-999999">
+                <AnimatePresence mode="popLayout">
+                {toasts.map((toast) => {
+                    const variant = variants[toast.type];
+                    const Icon = variant.icon;
 
-                return (
-                    <div
-                        key={toast.id}
-                        className={`bg-white text-offblack relative px-6 py-3 rounded-xl overflow-hidden shadow-primary flex justify-center items-center toastPresence min-w-[200px]`}
-                    >
-                        <div className="flex gap-3 justify-center items-center mb-1">
-                            <div className={`${variant.bgLight} w-9 h-9 rounded-full mt-0.5 flex justify-center items-center`}>
-                                <Icon className={`${variant.iconColor}`} size={variant.iconSize}/>
-                            </div>
+                    return (
+                        <motion.div
+                            layout="position"
+                            key={toast.id}
+                            className={`bg-offblack shadow-inner-light text-offwhite relative px-6 py-4 rounded-xl overflow-hidden shadow-primary min-w-[400px] flex justify-start`}
+                            initial={{ opacity: 0, x: 20 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 20 }}
+                            transition={{
+                                layout: {
+                                    type: "spring",
+                                    duration: 1.2,
+                                },
+                                x: {
+                                    type: "spring",
+                                    stiffness: 200,
+                                    damping: 18,
+                                    ease: "easeOut"
+                                },
+                                opacity: {
+                                    duration: 0.2,
+                                },
+                            }}>
 
+                            {/*gradient*/}
+                            <div className={`absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 w-[100px] h-[100px] rounded-full blur-[80px] ${variant.bg}`}/>
+
+                            <div className="flex gap-3 justify-center items-center mb-1 z-10">
+                                <div className={`${variant.bgLight} rounded-full p-2 flex justify-center items-center`}>
+                                    <div className={`${variant.bg} w-7 h-7 rounded-full mt-0.5 flex justify-center items-center`}>
+                                        <Icon className={`${variant.iconColor}`} size={variant.iconSize}/>
+                                    </div>
+                                </div>
                             <div className="flex flex-col justify-center items-start">
-                                <span className="font-bold -mb-2 text-xl">
-                                    {toast.message}
+
+                            <span className="font-bold -mb-2 text-xl">
+                                {toast.message}
+                            </span>
+
+                                    {toast.description && (
+                                        <span className="text-lg">
+                                    {toast.description}
                                 </span>
-
-                                {toast.description && (
-                                    <span className="text-lg">
-                                        {toast.description}
-                                    </span>
-                                )}
+                                    )}
+                                </div>
                             </div>
-                        </div>
 
-                        {/* Progress bar */}
-                        <div
-                            className={`${variant.bg} bottom-0 left-0 w-full h-1 absolute toastCountdown`}
-                        />
-                    </div>
-                );
-            })}
-        </div>
+                            {/* Progress bar */}
+                            <motion.div
+                                initial={{ width: "100%" }}
+                                animate={{ width: 0 }}
+                                transition={{ duration: 3, ease: "linear" }}
+                                className={`${variant.bg} bottom-0 left-0 w-full h-1 absolute`}
+                            />
+                        </motion.div>
+                    );
+                })}
+                </AnimatePresence>
+            </div>
     );
 }
